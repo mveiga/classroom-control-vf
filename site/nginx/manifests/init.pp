e5aa6c32dd1c49db2aa540478167ff0778393596
mveiga <mmveiga@gmail.com>
2016-03-29 15:17:58 -0400

    lab 11.2.1

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,22 @@
+class nginx {
+      package {'nginx':
+              ensure => present,
+              }
+      file { '/var/www':
+              ensure => directory,
+              require => Package['nginx'],
+            }
+      file { '/etc/nginx/nginx.conf':
+              ensure => file,
+              source => 'puppet:///modules/ngnix/nginx.conf',
+              require => Package['nginx'],
+            }
+      file { '/etc/nginx/conf.d/default.conf':
+              ensure => file,
+              source => 'puppet:///modules/ngnix/default.conf',
+              require => Package['nginx'],
+            }
+      service { 'nginx':
+                ensure => running,
+                enable => true,
+              }
