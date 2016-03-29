2343791431ad31fe9c8de0d6e07fd28808421961
mveiga <mmveiga@gmail.com>
2016-03-29 13:59:00 -0400

    11.1.2

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -5,7 +5,7 @@ class memcached {
       file {'/etc/sysconfig/memcached':
             ensure => file,
             source => 'puppet:///modules/memcached/memcached`,
-            require => Package [memcached],
+            require => Package ['memcached'],
            }
       service {'memcached':
                ensure => running,
