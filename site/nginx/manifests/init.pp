85484964cc89d5e9ea9e3e47a245db2a4584b940
mveiga <mmveiga@gmail.com>
2016-03-29 15:28:00 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -8,16 +8,17 @@ class nginx {
             }
       file { '/etc/nginx/nginx.conf':
               ensure => file,
-              source => 'puppet:///modules/ngnix/nginx.conf',
+              source => 'puppet:///modules/nginx/nginx.conf',
               require => Package['nginx'],
             }
       file { '/etc/nginx/conf.d/default.conf':
               ensure => file,
-              source => 'puppet:///modules/ngnix/default.conf',
+              source => 'puppet:///modules/nginx/default.conf',
               require => Package['nginx'],
             }
       service { 'nginx':
                 ensure => running,
                 enable => true,
+                subscribe => File ['/etc/nginx/nginx.conf'],
               }
 }
