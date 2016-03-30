d56b55169ee7e21d702af537424fe5b5a40212a7
mveiga <mmveiga@gmail.com>
2016-03-30 09:53:59 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,9 @@
 class nginx {
+      File {
+            owner => 'root',
+            group => 'root',
+            mode => '0644', 
+            }
       package {'nginx':
               ensure => present,
               }
