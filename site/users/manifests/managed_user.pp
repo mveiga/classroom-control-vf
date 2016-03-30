bcdc5f456e90c738e1bb78692c1963c862b0882c
mveiga <mmveiga@gmail.com>
2016-03-30 14:25:56 -0400

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,29 @@
+define user::managed_user  (
+        $username = $title,
+        $userhome = "/home/${title}",
+        $groupname = $title,
+        $sshdir = "${userhome}/.ssh"
+      ){
+        File {
+             owner => $username,
+             group => $groupname,
+             }
+        user { $username:
+                ensure => present,
+                home => $userhome,
+                gid => $groupname,
+              }
+        group { $groupname :
+                ensure => present,
+              }
+        file { $userhome: 
+               ensure => directory,
+               mode => 0755,
+              }
+         file { $sshdir: 
+               ensure => directory,
+               mode => 0700,
+               require => File ["$userhome"],
+              }
+               
+        }
