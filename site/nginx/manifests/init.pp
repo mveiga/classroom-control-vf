fbff2f6ab76ba9510bfffc065bef47fa6d6186fb
mveiga <mmveiga@gmail.com>
2016-03-30 10:16:48 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -18,13 +18,13 @@ class nginx {
               ensure => file,
               source => 'puppet:///modules/nginx/nginx.conf',
               require => Package['nginx'],
-              notify => Service ['nginx'],
+              notify => Service['nginx'],
             }
       file { '/etc/nginx/conf.d/default.conf':
               ensure => file,
               source => 'puppet:///modules/nginx/default.conf',
               require => Package['nginx'],
-              notify => Service ['nginx'],
+              notify => Service['nginx'],
             }
       service { 'nginx':
                 ensure => running,
